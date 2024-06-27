949b83407ce4cfba86881d15637881c0a15f06e4
pernyf <per.nyfelt@nordnet.se>
2024-06-27 16:03:00 +0200

    Recognize H2 modes and return the DataBaseProvider that is intended

--- src/main/groovy/se/alipsa/groovy/datautil/DataBaseProvider.groovy
+++ src/main/groovy/se/alipsa/groovy/datautil/DataBaseProvider.groovy
@@ -32,6 +32,30 @@ enum DataBaseProvider {
   }
 
   static DataBaseProvider fromUrl(String url) {
+    DataBaseProvider provider = findEnumMatch(url)
+    if (provider == H2 && url.contains('MODE=')) {
+      if (url.contains('MODE=DB2')) {
+        provider = DB2
+      } else if (url.contains('MODE=Derby')) {
+        provider = DERBY
+      } else if (url.contains('MODE=HSQLDB')) {
+        provider = HSQLDB
+      } else if (url.contains('MODE=MariaDB')) {
+        provider = MARIADB
+      } else if (url.contains('MODE=MySQL')) {
+        provider = MYSQL
+      } else if (url.contains('MODE=Oracle')) {
+        provider = ORACLE
+      } else if (url.contains('MODE=PostgreSQL')) {
+        provider = POSTGRESQL
+      } else if (url.contains('MODE=MSSQLServer')) {
+        provider = MSSQL
+      }
+    }
+    provider
+  }
+
+  private static DataBaseProvider findEnumMatch(String url) {
     for (DataBaseProvider db : values()) {
       if (url.startsWith(db.urlStart)) {
         return db
